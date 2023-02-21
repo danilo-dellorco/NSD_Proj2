4a0ef136913c64a42f518570671d0972e58a8f10
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2023-02-21 09:51:42 +0100

    Edited Central Node Start Script

--- docker/central-node/start.sh
+++ docker/central-node/start.sh
@@ -1,11 +1,12 @@
-# Configurazione Interfacce
-ip addr add  10.23.1.2/24 dev enp0s3
-
-# DOVREBBERO CONOSCERLA CON MPLS/BGP/VRF
-#ip route add 10.123.0.0/16 via 10.23.1.1 
+# Configurazione Interfacce e Rotte
+echo "Configuring Host Interfaces & Routes.."
+sudo ip route del 0/0 2>/dev/null
+sudo ip addr add 10.23.1.2/24 dev enp0s3 2>/dev/null
+sudo ip route add 10.123.0.0/16 via 10.23.1.1 2>/dev/null
+sudo ip route add 10.23.0.0/24 via 10.23.1.1 2>/dev/null
 
 # Avvio Servizio di NotifyWait per i nuovi file scaricati
-
+echo "Starting AV Listening Service.."
 inotifywait -m $(cat .conf/to_scan) -e create -e moved_to |
     while read dir action file; do
         echo "The executable '$file' is received in directory '$dir' via '$action'"
