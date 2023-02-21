59b4a8d734b73dc96f90f97420877275a9f45600
michelesalvv <michelesalvv@gmail.com>
2023-02-21 21:22:50 +0100

    CE configs updated

--- scripts/CEs/CE-A3.sh
+++ scripts/CEs/CE-A3.sh
@@ -1,7 +1,7 @@
 #! /bin/bash
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
-ip addr add 100.0.13.2/30 dev enp0s3
-ip addr add 10.23.1.1/24 dev enp0s8
+ip addr add 100.0.13.2/30 dev eth0
+ip addr add 10.23.1.1/24 dev eth1
 
 ip route add default via 100.0.13.1
