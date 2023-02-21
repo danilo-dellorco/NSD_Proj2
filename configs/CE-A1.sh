6a6214ffd1fc103b5ea827344e0b9ce084e6a935
michelesalvv <michelesalvv@gmail.com>
2023-02-21 10:07:14 +0100

    ce-a1 config update

--- configs/CE-A1.sh
+++ configs/CE-A1.sh
@@ -4,7 +4,6 @@
 echo 1 > /proc/sys/net/ipv4/ip_forward
 
 ip addr add 100.0.21.2/30 dev enp0s3
-ip route add default via 100.0.21.1
 
 
 ## MACSec
@@ -26,3 +25,6 @@ ipv4.method manual \
 ipv4.addresses 10.23.0.1/24
 
 nmcli connection up macsec-connection
+
+
+ip route add default via 100.0.21.1
