8062c5f49194f7b1554e0f2ab4bcc06c20e502b2
Danilo Dell'Orco <danilodellorcolp@gmail.com>
2023-02-21 10:01:49 +0100

    Refactor Branch before Merge

--- scripts/CEs/CE-A2.sh
+++
@@ -1,6 +0,0 @@
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-ip addr add 100.0.32.2/30 dev enp0s3
-ip addr add 10.123.0.1/16 dev enp0s8
-
-ip route add default via 100.0.32.1
--- scripts/CEs/CE-A3.sh
+++
@@ -1,6 +0,0 @@
-echo 1 > /proc/sys/net/ipv4/ip_forward
-
-ip addr add 100.0.13.2/30 dev enp0s3
-ip addr add 10.23.1.1/24 dev enp0s8
-
-ip route add default via 100.0.13.1
