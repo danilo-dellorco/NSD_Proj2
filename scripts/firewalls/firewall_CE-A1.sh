53b8181a9809bbe07e187eeb508e3fb22ccc0771
michelesalvv <michelesalvv@gmail.com>
2023-02-22 20:34:29 +0100

    Firewall Configurations updated

--- scripts/firewalls/firewall_CE-A1.sh
+++ scripts/firewalls/firewall_CE-A1.sh
@@ -2,14 +2,13 @@
 export EXT=enp0s3
 export LAN=macsec0
 
-# iptables without -t is implicit referred to filter table
-# flush every rule from every chain
+
 iptables -F
 
 # default behaviour
-iptables -P FORWARD DROP				# deny all traffic in forward hook
-iptables -P INPUT DROP					# deny all traffic in input hook
-iptables -P OUTPUT ACCEPT				# permit all traffic in output hook
+iptables -P FORWARD DROP				
+iptables -P INPUT DROP					
+iptables -P OUTPUT ACCEPT				
 
 # 1) permit traffic between LAN and outside with SNAT only if initiated by LAN
 iptables -A FORWARD -i $LAN -o $EXT -j ACCEPT
@@ -20,13 +19,10 @@ iptables -A POSTROUTING -t nat -o $EXT -j SNAT --to 10.23.0.2-10.23.0.254
 # 2) deny all traffic to GW except SSH and ICMP only if initated by LAN
 iptables -A INPUT -i $LAN -p tcp --dport 22 -j ACCEPT
 iptables -A INPUT -i $LAN -p icmp -j ACCEPT
-# deny traffic from GW to host if not initiated by LAN
-#iptables -A OUTPUT -o $LAN -p icmp -m state --state NEW -j DROP
-#iptables -A OUTPUT -o $LAN -p tcp --dport 22 -m state --state NEW -j DROP
 
 # 3) permit all traffic from GW to everywhere (and related response packets)
 iptables -A INPUT -m state --state ESTABLISHED,RELATED -j ACCEPT
 
 # 4) permit port forwarding with DNAT to hostA1 and hostA2 from outside only for HTTP service
 iptables -A FORWARD -i $EXT -o $LAN -p tcp --dport 80 -j ACCEPT
-iptables -A PREROUTING -t nat -i $EXT -p tcp --dport 80 -j DNAT --to 10.23.0.2-10.23.0.3
+iptables -A PREROUTING -t nat -i $EXT -p tcp --dport 80 -j DNAT --to 10.23.0.3
--- scripts/firewalls/firewall_CE-A2.sh
+++ scripts/firewalls/firewall_CE-A2.sh
@@ -4,18 +4,15 @@ export LAN=enp0s8
 export CENTRAL_NODE=10.23.1.2
 export AV1=10.123.0.2
 export AV3=10.123.0.4
-#AV2 with address 10.123.0.3
 
-# flush every rule from every chain
 iptables -F
 
-# default behavior drop everything
 iptables -P FORWARD DROP
 iptables -P INPUT DROP
 iptables -P OUTPUT DROP
 
 # 1) permit bidirectional traffic between central node and the AVs
-# more specific than the LAN IP range for AVs
+
 iptables -A FORWARD -i $EXT -o $LAN -s $CENTRAL_NODE -m iprange --dst-range $AV1-$AV3 -j ACCEPT
 iptables -A FORWARD -i $LAN -o $EXT -m iprange --src-range $AV1-$AV3 -d $CENTRAL_NODE -j ACCEPT
 
